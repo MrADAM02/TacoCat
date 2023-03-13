b90ecd83dcea211aba799a7c79cd8f3f3a05b14e
ADAM02 <75945467+MrADAM02@users.noreply.github.com>
2023-03-13 21:26:14 +0300

    Creating TacoCat App

--- js/site.js
+++ js/site.js
@@ -1,4 +1,53 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World")
+//get our user input 
+function getValue(){
+
+    //make sur the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+
+    //get user string for the page
+    let userString = document.getElementById("userString").value;
+
+    //check for a palindrom
+    let returnObj =  checkForPalindrome(userString);
+
+    //display out message to the screen
+    displayMessage(returnObj);
+}
+
+//check if first is a palindrome
+function checkForPalindrome(userString){
+    //change it to lower case
+    userString = userString.toLowerCase();
+
+    //remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = userString.length - 1 ; index >= 0; index--){
+        revString += userString[index];
+    }
+
+    if(revString == userString){
+        returnObj.msg = "Well done! You entered a palindrome!"
+    }
+    else{
+        returnObj.msg = "Sorry! You did not enter a palindrome!"
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+
+}
+
+//display a message to the string.
+function displayMessage(returnObj){
+
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
